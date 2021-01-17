a3d894e6e4d6263a1c63c6b3360b162047d975ba
Allan Commelin <commelinallan@gmail.com>
2021-01-17 19:25:13 +0100

    WIP register on node api

---
+++ src/store/constants/MusicsTypes.js
@@ -0,0 +1,69 @@
+const MusicTypes = [
+    {
+        'value': 'classical',
+        'label': 'Classique',
+        'checked': false,
+    },
+    {
+        'value': 'country',
+        'label': 'Country',
+        'checked': false,
+    },
+    {
+        'value': 'electro',
+        'label': 'Electro',
+        'checked': false,
+    },
+    {
+        'value': 'hip-hop',
+        'label': 'Hip-hop',
+        'checked': false,
+    },
+    {
+        'value': 'jazz',
+        'label': 'Jazz',
+        'checked': false,
+    },
+    {
+        'value': 'k-pop',
+        'label': 'K-pop',
+        'checked': false,
+    },
+    {
+        'value': 'metal',
+        'label': 'Métal',
+        'checked': false,
+    },
+    {
+        'value': 'pop',
+        'label': 'Pop',
+        'checked': false,
+    },
+    {
+        'value': 'rap',
+        'label': 'Rap',
+        'checked': false,
+    },
+    {
+        'value': 'rhythm&blues',
+        'label': 'Rhythm & blues',
+        'checked': false,
+    },
+    {
+        'value': 'rock',
+        'label': 'Rock',
+        'checked': false,
+    },
+    {
+        'value': 'techno',
+        'label': 'Techno',
+        'checked': false,
+    },
+    {
+        'value': 'reggae',
+        'label': 'Reggae',
+        'checked': false,
+    }
+];
+
+export default MusicTypes;
